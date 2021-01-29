ef50655b6c0e578a04bcca5174aa70ed03dd65f1
khalidpark2029 <yallayallakorea@gmail.com>
2021-01-29 15:27:49 +0000

    Challenge again

--- scrapper.py
+++ scrapper.py
@@ -3,12 +3,11 @@ from bs4 import BeautifulSoup
 
 URL = f'https://www.mangoplate.com/search/%EB%96%A1%EB%B3%B6%EC%9D%B4?keyword=%EB%96%A1%EB%B3%B6%EC%9D%B4&page='
 
-def get_current_page():
+def get_last_page():
   result = requests.get(URL)
   soup = BeautifulSoup(result.text, "html.parser")
   print(soup)
   pagination = soup.find("div", {"class" : "item_title"})
-  print(pagination)
 
 def get_lands():
-    current_page = get_current_page()
+    last_page = get_last_page()
